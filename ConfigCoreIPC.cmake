56c420243cf12b9c68ae57fd07becfc8718c1cc0
Edison Ai <edison.ai@arm.com>
2018-07-17 16:04:14 +0800

    Core: Add config file for IPC
    Add ConfigCoreIPC.cmake file for enabling IPC function,
    TFM_PSA_API and CORE_TEST_IPC depend on it.
    
    Change-Id: I6bcadfc3d7a8184e4c55aaf56e2958520be5a94a
    Signed-off-by: Edison Ai <edison.ai@arm.com>

---
+++ ConfigCoreIPC.cmake
@@ -0,0 +1,30 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2018, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+#This file holds information of a specific build configuration of this project.
+
+#Include board specific config (CPU, etc...), select platform specific build
+#system settings file
+if(NOT DEFINED TARGET_PLATFORM)
+	message(FATAL_ERROR "ERROR: TARGET_PLATFORM is not set in command line")
+elseif(${TARGET_PLATFORM} STREQUAL "AN521")
+	set(PLATFORM_CMAKE_FILE "${CMAKE_CURRENT_LIST_DIR}/platform/ext/Mps2AN521.cmake")
+elseif(${TARGET_PLATFORM} STREQUAL "AN519")
+	set(PLATFORM_CMAKE_FILE "${CMAKE_CURRENT_LIST_DIR}/platform/ext/Mps2AN519.cmake")
+else()
+	message(FATAL_ERROR "ERROR: Target \"${TARGET_PLATFORM}\" is not supported.")
+endif()
+
+#These variables select how the projects are built. Each project will set
+#various project specific settings (e.g. what files to build, macro
+#definitions) based on these.
+set (REGRESSION True)
+set (CORE_TEST False)
+set (CORE_IPC True)
+set (BL2 True)
+
+include ("${CMAKE_CURRENT_LIST_DIR}/CommonConfig.cmake")
