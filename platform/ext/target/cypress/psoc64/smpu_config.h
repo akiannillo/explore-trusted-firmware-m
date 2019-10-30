f835571428a3ab4b1f77e3c66f50be4928e2ff48
Chris Brand <chris.brand@cypress.com>
2019-10-30 13:42:17 -0700

    plat: PSoC 64 2MB SMPU settings
    Update the SMPU settings for the memory map of the 2MB PSoC64.
    
    Change-Id: I30c73f26284698a05069485678b80a7200dc1ad7

--- platform/ext/target/cypress/psoc64/smpu_config.h
+++ platform/ext/target/cypress/psoc64/smpu_config.h
@@ -39,15 +39,18 @@
     .pcMask = ONLY_BL2_SPM_MASK, \
 }
 
-/* SMPU0 - secondary image in Flash */
-#define SMPU0_BASE       S_ROM_ALIAS(0x00040000)
-#define SMPU0_REGIONSIZE PROT_SIZE_256KB_BIT_SHIFT
+/* SMPU0 - secure primary image in Flash */
+#define SMPU0_BASE          S_ROM_ALIAS(SECURE_IMAGE_OFFSET)
+#define SMPU0_REGIONSIZE    PROT_SIZE_512KB_BIT_SHIFT
+#define SMPU0_SUBREGION_DIS (CY_PROT_SUBREGION_DIS5 | \
+                             CY_PROT_SUBREGION_DIS6 | \
+                             CY_PROT_SUBREGION_DIS7)
 #define SMPU0_SLAVE_CONFIG {\
     .address = (void *)SMPU0_BASE, \
     .regionSize = SMPU0_REGIONSIZE, \
-    .subregions = ALL_ENABLED, \
-    .userPermission = CY_PROT_PERM_RW, \
-    .privPermission = CY_PROT_PERM_RW, \
+    .subregions = SMPU0_SUBREGION_DIS, \
+    .userPermission = CY_PROT_PERM_RWX, \
+    .privPermission = CY_PROT_PERM_RWX, \
     .secure = true, \
     .pcMatch = false, \
     .pcMask = SECURE_PCS_MASK, \
@@ -59,16 +62,23 @@
 #error "Flash layout has changed - SMPU0 needs updating"
 #endif
 
-/* SMPU1 - secure primary image in Flash */
-//#define SMPU1_BASE       (SMPU0_BASE + REGIONSIZE_TO_BYTES(SMPU0_REGIONSIZE))
-#define SMPU1_BASE       S_ROM_ALIAS(S_IMAGE_PRIMARY_PARTITION_OFFSET)
-#define SMPU1_REGIONSIZE PROT_SIZE_256KB_BIT_SHIFT
+/*
+ * SMPU0 protected area should be exactly the size of the secure primary image
+ */
+#if FLASH_S_PARTITION_SIZE != (5 * REGIONSIZE_TO_BYTES(SMPU0_REGIONSIZE)/8)
+#error "Flash layout has changed - FLASH_S_PARTITION_SIZE isn't 5/8 of SMPU0_REGIONSIZE"
+#endif
+
+/* SMPU1 - start of secondary images in Flash */
+#define SMPU1_BASE         S_ROM_ALIAS(NON_SECURE_IMAGE_OFFSET + \
+                                       NON_SECURE_IMAGE_MAX_SIZE)
+#define SMPU1_REGIONSIZE   PROT_SIZE_64KB_BIT_SHIFT
 #define SMPU1_SLAVE_CONFIG {\
     .address = (void *)SMPU1_BASE, \
     .regionSize = SMPU1_REGIONSIZE, \
     .subregions = ALL_ENABLED, \
-    .userPermission = CY_PROT_PERM_RWX, \
-    .privPermission = CY_PROT_PERM_RWX, \
+    .userPermission = CY_PROT_PERM_RW, \
+    .privPermission = CY_PROT_PERM_RW, \
     .secure = true, \
     .pcMatch = false, \
     .pcMask = SECURE_PCS_MASK, \
@@ -80,10 +90,12 @@
 #error "Flash layout has changed - SMPU1 needs updating"
 #endif
 
-/* SMPU2 - secure storage, NV counters, and unused space in Flash */
-//#define SMPU2_BASE       (SMPU1_BASE + REGIONSIZE_TO_BYTES(SMPU1_REGIONSIZE))
-#define SMPU2_BASE       S_ROM_ALIAS(FLASH_AREA_SCRATCH_OFFSET)
-#define SMPU2_REGIONSIZE PROT_SIZE_64KB_BIT_SHIFT
+/* SMPU2 - remainder of secondary images, plus secure storage, NV counters,
+ * and unused space in Flash
+ */
+/* SMPU2 should start immediately after SMPU1 */
+#define SMPU2_BASE         (SMPU1_BASE + REGIONSIZE_TO_BYTES(SMPU1_REGIONSIZE))
+#define SMPU2_REGIONSIZE   PROT_SIZE_512KB_BIT_SHIFT
 #define SMPU2_SLAVE_CONFIG {\
     .address = (void *)SMPU2_BASE, \
     .regionSize = SMPU2_REGIONSIZE, \
@@ -101,9 +113,9 @@
 #error "Flash layout has changed - SMPU2 needs updating"
 #endif
 
-/* SMPU6 - 32KB of unprivileged secure data in SRAM */
-#define SMPU6_BASE       S_DATA_START
-#define SMPU6_REGIONSIZE PROT_SIZE_32KB_BIT_SHIFT
+/* SMPU6 - 64KB of unprivileged secure data in SRAM */
+#define SMPU6_BASE         S_DATA_START
+#define SMPU6_REGIONSIZE   PROT_SIZE_64KB_BIT_SHIFT
 #define SMPU6_SLAVE_CONFIG {\
     .address = (void *)SMPU6_BASE, \
     .regionSize = SMPU6_REGIONSIZE, \
@@ -127,13 +139,12 @@
 #endif
 
 /* SMPU7 - 96KB of privileged secure data at S_DATA_PRIV_START in SRAM */
-#define SMPU7_BASE       S_RAM_ALIAS(0)
-#define SMPU7_REGIONSIZE PROT_SIZE_256KB_BIT_SHIFT
-#define SMPU7_SUBREGION_DIS    (CY_PROT_SUBREGION_DIS0 | \
-                                 CY_PROT_SUBREGION_DIS1 | \
-                                 CY_PROT_SUBREGION_DIS2 | \
-                                 CY_PROT_SUBREGION_DIS6 | \
-                                 CY_PROT_SUBREGION_DIS7)
+#define SMPU7_BASE          S_RAM_ALIAS(0)
+#define SMPU7_REGIONSIZE    PROT_SIZE_256KB_BIT_SHIFT
+#define SMPU7_SUBREGION_DIS (CY_PROT_SUBREGION_DIS0 | \
+                             CY_PROT_SUBREGION_DIS1 | \
+                             CY_PROT_SUBREGION_DIS6 | \
+                             CY_PROT_SUBREGION_DIS7)
 #define SMPU7_SLAVE_CONFIG {\
     .address = (void *)SMPU7_BASE, \
     .regionSize = SMPU7_REGIONSIZE, \
@@ -152,24 +163,24 @@
 #endif
 
 /*
- * S_DATA_PRIV_START must equal the base address of the subregion 3 of
+ * S_DATA_PRIV_START must equal the base address of subregion 2 of
  * SMPU7
  */
 #if S_DATA_PRIV_START != (SMPU7_BASE + \
-                          (3 * REGIONSIZE_TO_BYTES(SMPU7_REGIONSIZE) / 8))
-#error "Flash layout has changed - S_DATA_PRIV_START isn't subregion 3 of SMPU7"
+                          (2 * REGIONSIZE_TO_BYTES(SMPU7_REGIONSIZE) / 8))
+#error "Flash layout has changed - S_DATA_PRIV_START isn't subregion 2 of SMPU7"
 #endif
 
 /* SMPUs 6 and 7 should cover the whole secure data area in the RAM */
 #if S_DATA_SIZE != (REGIONSIZE_TO_BYTES(SMPU6_REGIONSIZE) + \
-                    3*REGIONSIZE_TO_BYTES(SMPU7_REGIONSIZE)/8)
+                    4*REGIONSIZE_TO_BYTES(SMPU7_REGIONSIZE)/8)
 #error "Flash layout has changed - SMPU6/SMPU7 config needs updating"
 #endif
 
 /* SMPU10 - 4KB of privileged executable data in SRAM
- * Note: Region resides in subregion 5 of SMPU 7*/
-#define SMPU10_BASE S_RAM_CODE_START
-#define SMPU10_REGIONSIZE PROT_SIZE_4KB_BIT_SHIFT
+ * Note: Region resides in subregion 4 of SMPU 7*/
+#define SMPU10_BASE         S_RAM_CODE_START
+#define SMPU10_REGIONSIZE   PROT_SIZE_4KB_BIT_SHIFT
 #define SMPU10_SLAVE_CONFIG {\
     .address = (void *)SMPU10_BASE, \
     .regionSize = SMPU10_REGIONSIZE, \
@@ -193,7 +204,7 @@
 
 /* SMPU10 should be contained within SMPU7 */
 #if (SMPU10_BASE + SMPU10_REGIONSIZE) < (SMPU7_BASE + \
-                    3*REGIONSIZE_TO_BYTES(SMPU7_REGIONSIZE)/8)
+                    2*REGIONSIZE_TO_BYTES(SMPU7_REGIONSIZE)/8)
 #error "SMPU10 is below SMPU7"
 #endif
 #if SMPU10_BASE > (SMPU7_BASE + \
